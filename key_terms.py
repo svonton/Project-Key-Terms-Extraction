d1cd7f3520cc4ff4861d217b53c14ef33ca19e39
svonton <56838368+svonton@users.noreply.github.com>
2021-06-09 15:33:45 +0300

    added spaces for better readability

--- key_terms.py
+++ key_terms.py
@@ -7,6 +7,7 @@ from nltk.stem import WordNetLemmatizer
 from string import punctuation
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 lemmatizer = WordNetLemmatizer()
 data = etree.parse('news.xml').getroot()
@@ -14,6 +15,7 @@ trash_can = stopwords.words('english') + list(punctuation)
 all_news = [news[1].text.replace('\n', ' ') for news in data[0]]
 ft_dataset = list()
 ft_data_header = list()
+
 for news in data[0]:
     tokenized_words = word_tokenize(news[1].text.lower())
     lem_words = [lemmatizer.lemmatize(word) for word in tokenized_words]
@@ -23,9 +25,11 @@ for news in data[0]:
     lem_words = [pos_tag([word])[0][0] for word in lem_words if pos_tag([word])[0][1] == 'NN']
     ft_data_header.append(news[0].text)
     ft_dataset.append(' '.join(lem for lem in lem_words))
+
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(ft_dataset)
 terms = vectorizer.get_feature_names()
+
 for i in range(len(ft_data_header)):
     print(ft_data_header[i] + ':')
     df = pd.DataFrame(tfidf_matrix[i].toarray())
